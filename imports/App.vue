4eea392f23b2b3f12af8ad15fb5c4a3ba6cbf07c
suvechhaya <suvechhaya@bajratechnologies.com>
2020-10-23 23:11:58 +0545

    Install Vue and replace default template provided by meteor by vue

---
+++ imports/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>{{ greeting }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {greeting: "Hello Meteor, from vue"};
+    }
+  };
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ imports/components/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">Hello World!</div>
+</template>
+
+<script>
+export default {
+  message () {
+    return { message1: 'Success!'};
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
