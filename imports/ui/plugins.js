7db47f5557f74baba677d72597e16831b62e794c
Suvu <shuvechha31@gmail.com>
2021-05-14 20:49:10 +0545

    Install bootstrap, popper.js and jquery and remove some unsed packages

--- imports/ui/plugins.js
+++ imports/ui/plugins.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
 import VueMeteorTracker from 'vue-meteor-tracker'
 
-Vue.use(VueMeteorTracker)
+import Bootstrap from 'bootstrap'
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../../node_modules/bootstrap/dist/js/bootstrap.min.js'
+
+
+Vue.use(VueMeteorTracker,Bootstrap)
