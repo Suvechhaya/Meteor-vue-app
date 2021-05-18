5ebc8b0c8a2a21387ce84cdfeca508ac32454f2b
Suvu <shuvechha31@gmail.com>
2021-05-18 14:02:20 +0545

    Add contents in aAbout page

--- imports/ui/components/About.vue
+++ imports/ui/components/About.vue
@@ -1,20 +1,39 @@
 <template>
-  <div>
-    <header>{{ title }}</header>
-    <p>This is About page.</p>
+  <div id="about" class="container p-5">
+    <div class="row">
+      <div class="col-sm-12 col-md-6 mt-4">
+        <h2>About Us</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="about-content">
+        <div class="col-sm-12 col-md-6">
+          <p class="text-justify">
+            Breathing exercises can help you relax, because they make your body
+            feel like it does when you are already relaxed. Deep breathing is
+            one of the best ways to lower stress in the body. This is because
+            when you breathe deeply, it sends a message to your brain to calm
+            down and relax.
+          </p>
+          <p class="text-justify">
+            This app can help you keep calm, restore your peace of mind and
+            increase your awareness. Along with these it can also be use as to
+            improve your concentration and ability to memorize.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "About",
-  data() {
-    return {
-      title: "About",
-    };
-  },
 };
 </script>
 
 <style scoped>
+h2 {
+  color: #333;
+}
 </style>
