44de69795e7dd5c21354ed62af6668de7b1b52ed
Suvu <shuvechha31@gmail.com>
2021-05-22 12:14:08 +0545

    Change about page design

--- imports/ui/components/About.vue
+++ imports/ui/components/About.vue
@@ -1,15 +1,13 @@
 <template>
   <div id="about" class="container p-5">
-    <div class="row">
-      <div class="col-sm-12 col-md-6 mt-4">
-        <h2>About Us</h2>
-      </div>
+    <div class="my-4 text-center">
+      <h2 class="text-primary">About Us</h2>
     </div>
     <div class="row">
-      <div class="about-content">
-        <div class="col-sm-12 col-md-6">
+      <div class="col-sm-12 col-md-6">
+        <div class="container p-4 about-bg">
           <p class="text-justify">
-            Breathing exercises can help you relax, because they make your body
+            <b>Breathing exercises</b> can help you relax, because they make your body
             feel like it does when you are already relaxed. Deep breathing is
             one of the best ways to lower stress in the body. This is because
             when you breathe deeply, it sends a message to your brain to calm
@@ -19,6 +17,16 @@
             This app can help you keep calm, restore your peace of mind and
             increase your awareness. Along with these it can also be use as to
             improve your concentration and ability to memorize.
+          </p>
+        </div>
+      </div>
+      <div class="col-sm-12 col-md-6">
+        <div class="container p-4 about-bg">
+          <p class="text-justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+            illo officia commodi vero exercitationem nihil libero maiores nam
+            sequi, praesentium dolor, modi eaque sed hic esse necessitatibus
+            quidem, suscipit aut.
           </p>
         </div>
       </div>
@@ -35,5 +43,8 @@ export default {
 <style scoped>
 h2 {
   color: #333;
+}
+.about-bg {
+  background-color: var(--blue-trans-light);
 }
 </style>
